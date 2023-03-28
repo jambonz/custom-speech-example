cc7ad04604ef39dd87af310c0f7e3799c78d5aef
Dave Horton <daveh@beachdognet.com>
2023-03-28 13:18:08 -0400

    more fixes from testing

--- build_stubs.sh
+++ build_stubs.sh
@@ -3,7 +3,7 @@
 mkdir -p ./stubs/vosk
 
 for FILE in ./protos/vosk/*; do 
-  grpc_tools_node_protoc   \
+  ./node_modules/.bin/grpc_tools_node_protoc   \
   --js_out=import_style=commonjs,binary:./stubs/vosk \
   --grpc_out=grpc_js:./stubs/vosk \
   --proto_path=./protos/vosk  \
